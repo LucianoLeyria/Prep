dac943ccea0f1b544f5297665b54e9de12db3f37
Luciano Leyria <87881395+LucianoLeyria@users.noreply.github.com>
2022-01-07 21:44:09 -0300

    Ejercicios js4 terminados

--- 04-JSIV/homework/homework.js
+++ 04-JSIV/homework/homework.js
@@ -29,19 +29,49 @@ function forEach(array, cb) {
   // Itera sobre el array "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada elemento el arreglo)
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    cb(array[i]);
+  }
 }
+
+// array.forEach(cb); // El forEach recorre el array (abreviacion de for (supongo).) debajo de ese array recorrido
+// } // existe una funcion (casos anteriores eran variables) el nombre de dicha funcion (cb)
+// es lo que le pasamos por parametro a forEach
+//forEach lo que hace es que te devuelve los elementos del array 1x1 !!
 
 function map(array, cb) {
   // Itera sobre cada elemento de "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   // Tu código:
+  var nuevoarr = [];
+  for (var i = 0; i < array.length; i++) {
+    nuevoarr.push(cb(array[i]));
+  }
+  return nuevoarr;
 }
+
+// Este lo hice todo  pero me olvidaba del return (vi las respuestas)
+// } // (dentro del cb esta value (el valor del array . ej [1,2,3] los values serian 0,1,2))
+// esta index que es lo escrito dentro del array y esta array que es el array completo.
+// y el map te devuelve un nuevo array con los nuevos resultados (podes dividir, multiplicar, etc)
 
 function filter(array) {
   // Filtrar todos los elementos del array que comiencen con la letra "a".
   // Devolver un nuevo array con los elementos que cumplen la condición
   // Tu código:
+  var nuevoarr = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][0] === "a") {
+      nuevoarr.push(array[i]);
+    }
+  }
+  return nuevoarr;
 }
+
+// return array.filter((arr) => arr[0] === "a");
+// filter lo que hace es que te filtra los elementos del array
+//  depende de la condicion que le pongas
+//}
 
 // ---- Ejercicios de Repaso ----
 
@@ -55,6 +85,7 @@ function deObjetoArray(objeto) {
       C: 3
      }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   // Tu código:
+  return Object.entries(objeto);
 }
 
 function numberOfCharacters(string) {
@@ -62,6 +93,15 @@ function numberOfCharacters(string) {
   // en formato par clave-valor.
   // Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
   // Tu código:
+  var nuevoobj = {};
+  for (var i = 0; i < string.length; i++) {
+    if (nuevoobj[string[i]]) {
+      nuevoobj[string[i]] = nuevoobj[string[i]] + 1; // aca estoy reasignando la propiedad
+    } else {
+      nuevoobj[string[i]] = 1; // aca la estamos creando
+    }
+  }
+  return nuevoobj;
 }
 
 function capicua(numero) {
@@ -69,12 +109,27 @@ function capicua(numero) {
   // La misma debe retornar: "Es capicua" si el número se lee igual de
   // izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   // Tu código:
+  var numastr = numero.toString().split("").reverse().join("");
+  var numnuevo = parseInt(numastr);
+  if (numero === numnuevo) {
+    return "Es capicua";
+  }
+  return "No es capicua";
 }
+// se que Math.floor (7.2) significa que redondea para abajo, por mas que sea 7.9 , el resultado va a ser 7
+// se que numero.toString() significa que el numero ej : 12321 lo pasamos a "12321"
 
 function deleteAbc(cadena) {
   // Define una función que elimine las letras "a", "b" y "c" de la cadena dada
   // y devuelva la versión modificada o la misma cadena, en caso de no contener dichas letras.
   // Tu código:
+  var nueva = "";
+  for (var i = 0; i < cadena.length; i++) {
+    if (cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c") {
+      nueva = nueva + cadena[i];
+    }
+  }
+  return nueva;
 }
 
 function buscoInterseccion(arreglo1, arreglo2) {
@@ -83,6 +138,13 @@ function buscoInterseccion(arreglo1, arreglo2) {
   // Si no tienen elementos en común, retornar un arreglo vacío.
   // Aclaración: los arreglos no necesariamente tienen la misma longitud
   // Tu código:
+  var nuevoarr = [];
+  for (var i = 0; i < arreglo1.length; i++) {
+    for (var k = 0; k < arreglo2.length; k++) {
+      if (arreglo1[i] === arreglo2[k]) nuevoarr.push(arreglo1[i]);
+    }
+  }
+  return nuevoarr;
 }
 
 // No modificar nada debajo de esta línea
